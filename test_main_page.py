508b3c7b09a5407f3450c24f990d312501faeec0
Serg1328 <splahin@gmail.com>
2022-05-30 16:29:24 +0300

    +add locators, login_page. modified base_page, main_page, twst_main_page

--- test_main_page.py
+++ test_main_page.py
@@ -1,4 +1,6 @@
+from .pages.login_page import LoginPage
 from .pages.main_page import MainPage
+
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
@@ -13,3 +15,27 @@ def test_guest_should_see_login_link(browser):
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+
+def test_guest_should_be_login_url(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    login_url = LoginPage(browser, browser.current_url)
+    login_url.should_be_login_url()
+
+def test_guest_should_be_login_form(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    login_form = LoginPage(browser, browser.current_url)
+    login_form.should_be_login_form()
+
+def test_guest_should_be_register_form(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    register_form = LoginPage(browser, browser.current_url)
+    register_form.should_be_register_form()
