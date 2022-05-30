3e1e5d5e9dacc3b57334c1042280f6a2fcbff68c
Serg1328 <splahin@gmail.com>
2022-05-30 17:36:30 +0300

    modified main_page, test_main_page

--- test_main_page.py
+++ test_main_page.py
@@ -7,7 +7,9 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)
     # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()                      # открываем страницу
-    page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
 
 
 def test_guest_should_see_login_link(browser):
@@ -15,27 +17,3 @@ def test_guest_should_see_login_link(browser):
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
-
-def test_guest_should_be_login_url(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
-    page.open()
-    page.go_to_login_page()
-    login_url = LoginPage(browser, browser.current_url)
-    login_url.should_be_login_url()
-
-def test_guest_should_be_login_form(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
-    page.open()
-    page.go_to_login_page()
-    login_form = LoginPage(browser, browser.current_url)
-    login_form.should_be_login_form()
-
-def test_guest_should_be_register_form(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
-    page.open()
-    page.go_to_login_page()
-    register_form = LoginPage(browser, browser.current_url)
-    register_form.should_be_register_form()
