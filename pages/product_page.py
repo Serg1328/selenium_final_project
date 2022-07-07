7b04777fdf29cf62acdc912e32dd5b63122fddd9
Serg1328 <splahin@gmail.com>
2022-07-07 20:15:44 +0300

    add negative tests

--- pages/product_page.py
+++ pages/product_page.py
@@ -6,11 +6,7 @@ class ProductPage(BasePage):
     def add_to_basket(self):
         link = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET)
         link.click()
-        self.solve_quiz_and_get_code()
-        self.should_be_added_to_basket()
-        self.should_be_name_product_added_to_basket_as_same_name_product_on_product_page()
-        self.should_be_cart_price_message()
-        self.should_be_cart_prise_as_same_price_product()
+
 
     def should_be_added_to_basket(self):
         assert self.is_element_present(*ProductPageLocators.ADDED_TO_BASKET), "product not added"
@@ -27,3 +23,11 @@ class ProductPage(BasePage):
         price_product = self.browser.find_element(*ProductPageLocators.PRICE_PRODUCT).text
         cart_price = self.browser.find_element(*ProductPageLocators.CART_PRICE).text
         assert price_product == cart_price, "cart price differs from price on product page"
+
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message is presented, but should not be"
+
+    def should_disappear_of_success_message(self):
+        assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message is presented, but should not be"
--- test_product_pages.py
+++ test_product_pages.py
@@ -1,11 +1,11 @@
 import pytest
 from .pages.product_page import ProductPage
 
-
-
+url_neg = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/"
 base_url = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207?promo=offer"
 
-#@pytest.mark.parametrize('number_offer', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+
+# @pytest.mark.parametrize('number_offer', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
 #                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
 #                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
 #                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
@@ -15,10 +15,37 @@ base_url = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207?p
 #                                  pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail(reason="product name is different {name_product} != {name_product_message}")),
 #                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
 #                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
-@pytest.mark.parametrize('number_offer', [pytest.param(n, marks=pytest.mark.xfail(n==7, reason="product name is different {name_product} != {name_product_message}")) for n in range(10)])
+@pytest.mark.parametrize('number_offer', [pytest.param(n, marks=pytest.mark.xfail(n == 7,
+                                                                                  reason="product name is different {name_product} != {name_product_message}"))
+                                          for n in range(10)])
 def test_guest_can_add_product_to_basket(browser, number_offer):
-    #url = number_offer
+    # url = number_offer
     url = f"{base_url}{number_offer}"
     page = ProductPage(browser, url)
     page.open()
     page.add_to_basket()
+    page.solve_quiz_and_get_code()
+    page.should_be_added_to_basket()
+    page.should_be_name_product_added_to_basket_as_same_name_product_on_product_page()
+    page.should_be_cart_price_message()
+    page.should_be_cart_prise_as_same_price_product()
+
+@pytest.mark.xfail(reason="")
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    page = ProductPage(browser, url_neg, 0)
+    page.open()
+    page.add_to_basket()
+    page.should_not_be_success_message()
+
+
+def test_guest_cant_see_success_message(browser):
+    page = ProductPage(browser, url_neg, 0)
+    page.open()
+    page.should_not_be_success_message()
+
+@pytest.mark.xfail(reason="")
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    page = ProductPage(browser, url_neg, 0)
+    page.open()
+    page.add_to_basket()
+    page.should_disappear_of_success_message()
